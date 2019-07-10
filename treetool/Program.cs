b865c306bb2f18abc04163fa40588c24dc4bd8d9
Olga Roslyakova <oroslyakova@game-forest.com>
2019-07-10 18:33:28 +1000

    уже лучше

--- treetool/Program.cs
+++ treetool/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,7 +16,7 @@ namespace treetool
             DirectoryInfo dir = new DirectoryInfo(path);
             this.depth = depth;
                  
-            OpenFolder(dir, 1, showSize, readableSize);
+            OpenFolder(dir, 0, showSize, readableSize);
         }
 
         private void OpenFolder(DirectoryInfo curDir, int depthCurrent, bool showSize, bool readableSize)
@@ -28,16 +27,20 @@ namespace treetool
                 foreach (var file in curDir.EnumerateFiles()) {
                     try {
                         Console.WriteLine($"{Tabs(depthCurrent - 1)}|───{file.Name} {Size(file, showSize, readableSize)}");
-                    } catch (System.UnauthorizedAccessException e) { 
-                    } finally { }
+                    } catch (UnauthorizedAccessException e) {
+                        Console.WriteLine($"{Tabs(depthCurrent - 1)}|───Недоступный файл");
+                    } catch (Exception e) {
+                    }
                 }
 
                 foreach (var dir in curDir.EnumerateDirectories()) {
                     try {
                         Console.WriteLine($"{Tabs(depthCurrent - 1)}└───{dir.Name}");
                         OpenFolder(dir, depthCurrent + 1, showSize, readableSize);
-                    } catch (System.UnauthorizedAccessException e) {
-                    } finally { }
+                    } catch (UnauthorizedAccessException e) {
+                        Console.WriteLine($"{Tabs(depthCurrent - 1)}|───Недоступная папка");
+                    } catch (Exception e) {
+                    }
                 }
             }
         }
@@ -66,38 +69,37 @@ namespace treetool
         {
             return new String(' ', n*4);
         }
-
-        
     }
 
     class Program
     {
         static bool Contains(string str, string substr)
         {
-            return (str.IndexOf(substr) != -1) ? true : false;
+            return (str.IndexOf(substr) != -1);
         }
         static bool Contains(string str, string substr1, string substr2)
         {
-            return (str.IndexOf(substr1) != -1) || (str.IndexOf(substr2) != -1) ? true : false;
+            return (str.IndexOf(substr1) != -1) || (str.IndexOf(substr2) != -1);
         }
 
         static void WriteInfo()
         {
-            Console.Write("Info:{0}exit - для выхода{0}Дополнительные аргументы при запуске приложения, которые могут быть использованы все вместе:{0}задают глубину вложенности(-d или--depth){0}показывают размер объектов(-s или--size),{0}в том числе удобном для восприятия виде(-h или--human - readable){0}справка по использованию(--help или -?)\n", "\n    ");
+            Console.Write(
+                "Info:{0}Команды и параметры вводятся в одну строку через пробел!{0}exit - для выхода{0}Дополнительные аргументы при запуске приложения, которые могут быть использованы все вместе:{0}задают глубину вложенности(-d или--depth) далее через пробел глубина {0}(нумерация от 0, то есть 0 не раскрывает папки в текущем каталоге){0}показывают размер объектов(-s или--size),{0} в том числе удобном для восприятия виде(-h или--human - readable){0}справка по использованию(--help или -?)\n", "\n    ");
         }
 
         static int Depth(string str)
         {
             var numstr = str.Substring(str.IndexOf(' ', str.IndexOf("-d")) + 1);
-            numstr = numstr.Substring(0, numstr.IndexOf(' '));
-            int num = 0;
-            if (numstr.Length < 1)
-                return num;
-            int.TryParse(numstr, out num);
-            if (num < 1)
-                return 1;
+            if (Contains(numstr, " ")) 
+                numstr = numstr.Substring(0, numstr.IndexOf(' '));
             else
-                return num;
+                numstr = numstr.Substring(0, numstr.Length);
+            if (numstr.Length < 1)
+                return 0;
+            int num = 0;
+            int.TryParse(numstr, out num);
+            return num;
         }
 
         static void Main(string[] args)
